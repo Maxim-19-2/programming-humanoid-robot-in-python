440430336c3a9f0c1177bc5fd2809efeceedb8ae
Maxim-19-2 <ms.smoljar@gmail.com>
2022-05-24 15:23:41 +0200

    pid controller updated

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 25
+        self.Ki = 0.1
+        self.Kd = 0.1
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,6 +53,13 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
+        #Formel aus der Vorlesung - Folie 7 (joint control)
+        error = target - sensor
+
+        self.u = self.u + ((self.Kp + (self.Ki*self.dt) + (self.Kd/self.dt)) * error) - ((self.Kp + ((2*self.Kd)/self.dt))*self.e1) + ((self.Kd/self.dt)*self.e2)
+        
+        self.e2 = self.e1
+        self.e1 = error
 
         return self.u
 
